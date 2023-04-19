5d046665e4102cc9935d36311ca92f1b8ae7efc9
Jorgen <jorgen@buildnode.io>
2023-04-19 10:28:38 -0700

    refactor: error message

--- node/os.ts
+++ node/os.ts
@@ -4,7 +4,7 @@ export function verifyOS(): void {
   const isMac = os.platform() === "darwin";
   if (!isMac)
     throw new Error(
-      "Verification must be performed on macos! The easiest way to do this is with a github action (see https://github.com/CycleOperators/BalanceCheckerVerification). This is due to differences in build environments resulting in different wasm hashes."
+      "Verification must be performed on macos! The easiest way to do this is with a github action. See https://github.com/CycleOperators/BalanceCheckerVerification for github action instructions. This is due to differences in build environments resulting in different wasm hashes."
     );
 }
 

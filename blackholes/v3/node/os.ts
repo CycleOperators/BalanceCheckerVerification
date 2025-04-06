7c4f611fe4bb37fdc92d729d7ae4574d467399a0
byronbecker <byron.becker@colorado.edu>
2025-04-05 21:50:29 -0700

    fix v3 action to cd into the right directory

--- blackholes/v3/node/os.ts
+++ blackholes/v3/node/os.ts
@@ -1,13 +1,11 @@
 import os from "os";
 
 export function verifyOS(): void {
-  /*
   const isMac = os.platform() === "linux";
   if (!isMac)
     throw new Error(
       "Verification must be performed on linux! The easiest way to do this is with a github action, or by following the docker instructions in this readme. See https://github.com/CycleOperators/BalanceCheckerVerification for github action instructions. This is due to differences in build environments resulting in different wasm hashes."
     );
-  */
 }
 
 if (!module.parent) verifyOS();
